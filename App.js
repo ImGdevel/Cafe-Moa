d43b91ad501bc3c4f6320d5003313010f3be3009
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-09-26 18:42:47 +0900

    Test Upload
    testing the upload

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/Home';
-import DetailsScreen from './screens/Details';
-import SignUpScreen from './screens/SignUp';
-import SettingsScreen from './screens/Settings';
-import ProfileScreen from './screens/Profile';
+import * as React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
+import SignUpScreen from "./screens/SignUp";
+import SettingsScreen from "./screens/Settings";
+import ProfileScreen from "./screens/Profile";
 
 const Tab = createBottomTabNavigator();
 const SettingsStack = createNativeStackNavigator();
 const HomeStack = createNativeStackNavigator();
-
+// 하윙
 export default function App() {
   return (
     <NavigationContainer>
@@ -44,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
