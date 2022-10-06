c5c912e2fb236c0b8824547725ce4cae6f12c718
eunaJ <104022743+eunaJ@users.noreply.github.com>
2022-10-06 17:55:56 +0900

    initial mypage ui

--- screens/InApp/MyPage.js
+++ screens/InApp/MyPage.js
@@ -1,32 +1,45 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { CurrentRenderContext } from '@react-navigation/native';
+import React from 'react';
+import { Image, Dimensions, TouchableOpacity, View, Text, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
 
 function MyPageScreen({ navigation }) {
+  function GoToOptionScreen(){
+    //navigation.navigate('#')
+  };
+
+  function GoToEditProfileScreen(){
+    //
+  };
+
+  function GoToLogoutScreen(){
+    //
+  };
+
+  function GoToDeleteAccountScreen(){
+    //
+  };
+
+
   return (
-    <KeyboardAvoidingView style={styles.container}>
-    {/* 여기부터 수정 구역(KeyboardAvoidingView 포함) */}
-    <View style={styles.contantArea}> {/*필요?*/}
-    <View style={styles.profileText}><Text style={{ fontWeight: "500", fontSize: 40 }}>마이페이지</Text></View>
+  <KeyboardAvoidingView style={styles.container}>
+    <View style={styles.myPageText}><Text style={{ fontWeight: "450", fontSize: 20 }}>마이페이지</Text></View>
+      
+    <View style={styles.upContentContainer}>
+      <Image style={styles.profilePicture} source={require('../../img/initialProfile.jpg')}></Image>
+        <View style={styles.idText}><Text style={{fontWeight: "400", fontSize: 20}}>아이디</Text></View>
     </View>
 
-    <View style={styles.profilePicture}>
-    </View>
-
-    <View style={styles.idText}><Text style={{fontSize: 20}}>{...id}</Text></View>
-
-    <View style={styles.horizeLine}></View>
-
-    <View style={styles.btnArea}>
-      <TouchableOpacity style={styles.btnOption} onPress = {GoToOptionScreen}>
+    <View style={styles.contentContainer}>
+      <TouchableOpacity style={styles.btnStyle} onPress = {GoToOptionScreen}>
         <Text style = {{color: 'black', fontSize: 20,}}>옵션</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btnEditProfile} onPress = {GoToEditProfileScreen}>
+      <TouchableOpacity style={styles.btnStyle} onPress = {GoToEditProfileScreen}>
         <Text style = {{color: 'black', fontSize: 20,}}>개인정보변경</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btnLogout} onPress = {GoToLogoutScreen}>
+      <TouchableOpacity style={styles.btnStyle} onPress = {GoToLogoutScreen}>
         <Text style = {{color: 'black', fontSize: 20,}}>로그아웃</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btnDeleteAccount} onPress = {GoToDeleteAccountScreen}>
+      <TouchableOpacity style={styles.btnStyle} onPress = {GoToDeleteAccountScreen}>
         <Text style = {{color: 'black', fontSize: 20,}}>회원탈퇴</Text>
       </TouchableOpacity>
     </View>
@@ -42,40 +55,50 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
-  //여기부터 수정
-  profileText: {
+
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  upContentContainer: {
+    flexDirection: 'row',
+    marginBottom: 80,
+  }, 
+
+  myPageText: {
+    marginTop: 40,
+  },
+
+  profilePicture: {
+    marginTop: 30,
+    marginLeft: -100,
     width: 100,
     height: 100,
-    border: 15,
-    borderRadius: '50%',
+    borderRadius: 75,
+    backgroundColor: 'white',
   },
 
   idText: {
-    /*필요한가?*/
+    marginVertical: 60,
+    marginLeft: 60,
   },
-
-  horizeLine: {
-    border: 0,
-    width: '50%',
-    height: 10,
-    backgroundColor: '#black',
-  },
-
-  btnArea: {
-    width: '100%',
-    height: 100,
-  },
-
-  btnOption, btnEditProfile, btnLogout, btnDeleteAccount: {
-    margin: 1,
-    width: '100%',
+  /*btnOption, btnEditProfile, btnLogout, btnDeleteAccount*/
+  btnStyle: {
+    marginVertical: -1,
+    width: Dimensions.get('window').width, /*360*/
     height: 60,
-    borderRadius: 7,
+    borderWidth: 1,
+    borderBottomColor: 'black',
+    borderLeftColor: 'white', 
+    borderRightColor: 'white',
+    borderColor: 'black',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#white',
+    backgroundColor: 'white',
+    fontSize: 20,
   },
 });
 
