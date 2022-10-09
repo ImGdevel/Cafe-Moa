1d88d605a471756f0c54eb1baeb8cb9b83d95635
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-10-09 10:00:20 +0900

    Correct Spell

--- screens/Reservation/Imfomation.js
+++ screens/Reservation/Imfomation.js
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import * as React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
 
 function ImfomationScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>카폐 정보 페이지 입니다.</Text>
-      
-      
-      
-      
-      <Button title="카페예약 페이지로->"onPress={() => navigation.navigate('Reservaion')}/>
+
+      <Button
+        title="카페예약 페이지로->"
+        onPress={() => navigation.navigate("Reservation")}
+      />
     </View>
   );
-} 
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
