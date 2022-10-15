c50b8be5532e58bb6b0875577c1442fbd1441e48
eunaJ <104022743+eunaJ@users.noreply.github.com>
2022-10-15 09:55:09 +0900

    not firebase connect

--- FireServer.js
+++ FireServer.js
@@ -3,12 +3,18 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDa6DPB6A3oRKbI8zVkfPJAow81fDJjRkY",
+  /*apiKey: "AIzaSyDa6DPB6A3oRKbI8zVkfPJAow81fDJjRkY",
   authDomain: "cafemoa-f9244.firebaseapp.com",
   projectId: "cafemoa-f9244",
   storageBucket: "cafemoa-f9244.appspot.com",
   messagingSenderId: "571985972680",
-  appId: "1:571985972680:web:acfbb695ab86f008d004fd"
+  appId: "1:571985972680:web:acfbb695ab86f008d004fd"*/
+  apiKey: "AIzaSyAmbcMo8zf8CHuV0l6FoLwk0VmIYH1fkQA",
+  authDomain: "test-9fd29.firebaseapp.com",
+  projectId: "test-9fd29",
+  storageBucket: "test-9fd29.appspot.com",
+  messagingSenderId: "12706083630",
+  appId: "1:12706083630:web:57d4e48975434da98780bf"
 };
 
 // Initialize Firebase
