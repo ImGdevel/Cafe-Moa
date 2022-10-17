e1c9e8cc143ed63d0c7d772020d7e4e1ecb3ba97
warmzer0 <114395452+warmzer0@users.noreply.github.com>
2022-10-17 19:18:15 +0900

    Update Home.js

--- screens/InApp/Home.js
+++ screens/InApp/Home.js
@@ -9,9 +9,14 @@ import {
 import getHomeStyle from "../../styles/screens/HomeStyle";
 
 function HomeScreen({ navigation }) {
-  function NearbyCafe() {
+
+  function ReservationInformation(){
     //navigation.navigate()
   }
+
+  function NearbyCafe(){
+
+  };
 
   function ConfirmReservation() {
     //
@@ -22,33 +27,73 @@ function HomeScreen({ navigation }) {
   }
 
   return (
-    <KeyboardAvoidingView style={getHomeStyle.container}>
-      <View style={getHomeStyle.homeText}>
-        <Text style={{ fontWeight: "700", fontSize: 65 }}>CAFE MOA</Text>
-      </View>
-
-      <View style={getHomeStyle.contentContainer}>
-        <TouchableOpacity
-          style={getHomeStyle.btnNearbyCafe}
-          onPress={() => navigation.navigate("Cafe")}
-        >
-          <Text style={{ color: "black", fontSize: 45 }}>주변카페</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={getHomeStyle.btnConfirmReservation}
-          onPress={() => navigation.navigate("Reservation")}
-        >
-          <Text style={{ color: "black", fontSize: 45 }}>예약 확인</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={getHomeStyle.btnMyPage}
-          onPress={() => navigation.navigate("MyPage")}
-        >
-          <Text style={{ color: "black", fontSize: 45 }}>마이 페이지</Text>
-        </TouchableOpacity>
-      </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.homeText}><Text style={{ color: '#828282', fontWeight: "700", fontSize: 50 }}>CAFE MOA</Text></View>
+      
+      <View style={styles.contentContainer}>
+      <TouchableOpacity style={styles.ReservationInformation} onPress = {() => navigation.navigate('Reservation')}>
+        <Text style = {{color: '#828282', fontSize: 20,}}> 00 예약정보</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnNearbyCafe} onPress = {() => navigation.navigate('Cafe')}>
+        <Text style = {{color: '#828282', fontSize: 45,}}>주변카페</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnConfirmReservation} onPress = {() => navigation.navigate('Reservation')}>
+        <Text style = {{color: '#828282', fontSize: 45,}}>예약 확인</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnMyPage} onPress = {() => navigation.navigate('MyPage')}>
+        <Text style = {{color: '#828282', fontSize: 45,}}>마이 페이지</Text>
+      </TouchableOpacity>
+    </View>
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 3,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  homeText: {
+    marginTop: '20%',
+    marginBottom: '20%',
+  },
+  
+  btnNearbyCafe:{
+    width: Dimensions.get('window').width, 
+    height: '20%', 
+    borderTopWidth: 2,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  btnConfirmReservation:{
+    width: Dimensions.get('window').width, 
+    height: '20%', 
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  btnMyPage:{
+    width: Dimensions.get('window').width, 
+    height: '20%', 
+    borderTopWidth: 1,
+    borderBottomWidth: 2,
+    borderColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  }
+
+});
 
 export default HomeScreen;
