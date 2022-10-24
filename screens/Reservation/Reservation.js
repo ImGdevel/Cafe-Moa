7e95a31d63f9ec3e707e55b0170e5c63ec8a8319
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-10-24 16:59:35 +0900

    Add timeArr

--- screens/Reservation/Reservation.js
+++ screens/Reservation/Reservation.js
@@ -13,6 +13,23 @@ function ReservationScreen({ navigation }) {
   const [modalVisible, setModalVisible] = useState(true);
   const [modalOutput, setModalOutput] = useState("Open Modal");
 
+  const timeArr = [
+    "09:00",
+    "09:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+  ];
+
   return (
     <View style={getReserveStyle.container}>
       <Modal
