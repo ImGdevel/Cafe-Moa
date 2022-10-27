aa2c45949f59784725f20cd029b0dc6484b8ab7e
eunaJ <104022743+eunaJ@users.noreply.github.com>
2022-10-27 22:19:01 +0900

    userdata update

--- FireServer.js
+++ FireServer.js
@@ -4,12 +4,19 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDa6DPB6A3oRKbI8zVkfPJAow81fDJjRkY",
-  authDomain: "cafemoa-f9244.firebaseapp.com",
-  projectId: "cafemoa-f9244",
-  storageBucket: "cafemoa-f9244.appspot.com",
-  messagingSenderId: "571985972680",
-  appId: "1:571985972680:web:acfbb695ab86f008d004fd"
+  // 유나
+  apiKey: "AIzaSyAmbcMo8zf8CHuV0l6FoLwk0VmIYH1fkQA",
+  authDomain: "test-9fd29.firebaseapp.com",
+  projectId: "test-9fd29",
+  storageBucket: "test-9fd29.appspot.com",
+  messagingSenderId: "12706083630",
+  appId: "1:12706083630:web:57d4e48975434da98780bf"
+  // apiKey: "AIzaSyDa6DPB6A3oRKbI8zVkfPJAow81fDJjRkY",
+  // authDomain: "cafemoa-f9244.firebaseapp.com",
+  // projectId: "cafemoa-f9244",
+  // storageBucket: "cafemoa-f9244.appspot.com",
+  // messagingSenderId: "571985972680",
+  // appId: "1:571985972680:web:acfbb695ab86f008d004fd"
 };
 
 // Initialize Firebase
@@ -24,3 +31,5 @@ export const dbService = firebase.firestore();
 export const authService = firebase.auth();
 export const storageService = firebase.storage();
 export const GoogleMapApiKey = MapApiKey();
+
+export const deletefield = firebase.firestore.FieldValue.delete(); 
