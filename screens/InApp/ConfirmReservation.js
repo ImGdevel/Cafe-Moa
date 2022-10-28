6c5b775f5a72315038119c4d68e169fe87390680
warmzer0 <114395452+warmzer0@users.noreply.github.com>
2022-10-28 22:05:19 +0900

    update ConfirmReservation

--- screens/InApp/ConfirmReservation.js
+++ screens/InApp/ConfirmReservation.js
@@ -1,48 +1,157 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
-  Button,
   View,
   Text,
-  StyleSheet,
-  KeyboardAvoidingView,
+  Image,
   TouchableOpacity,
-  Dimensions,
+  ScrollView,
+  FlatList,
 } from "react-native";
 
 import getConfirmReservationStyle from "../../styles/screens/ConfirmReservationStyle";
+import getCafeTableStyle from "../../styles/components/CafeTableStyle";
+import getFindStyle from "../../styles/components/FindStyle";
+
+const imgArr = [
+  {
+   idx: "12",
+   src: require("../../img/coffeebayLogo_test.jpg"),
+  },
+];
 
 function ConfirmReservationScreen({ navigation }) {
-  function InfoReservation() {
-    //navigation.navigate()
-  }
+  const [direction, setDirection] = useState("예약 내역");
 
   return (
-    <KeyboardAvoidingView style={getConfirmReservationStyle.container}>
-      <View style={getConfirmReservationStyle.homeText}>
-        <Text style={{ fontWeight: "700", fontSize: 65 }}>
-          Info Your Reservation
-        </Text>
-      </View>
+    <>
+      <View style={getConfirmReservationStyle.container}>
+        <View style={getFindStyle.container}>
+          <View style={getFindStyle.contentContainer}>
+            <CafeTable
+              name={"Coffee Bay"}
+              location={"용인시 처인구"}
+              imgae={""}
+              information={"Open : AM 09:00 || Close : PM 22:00"}
+            />
+          </View>
+        </View>
 
-      <View style={getConfirmReservationStyle.contentContainer}>
-        <TouchableOpacity
-          style={getConfirmReservationStyle.btnInfoReservation}
-          onPress={() => navigation.navigate("Reservation")}
-        >
-          <Text style={{ color: "#ccc", fontSize: 30, margin: 10 }}>
-            카페 위치 및 이름 :
-          </Text>
-          <Text style={{ color: "#ccc", fontSize: 20, margin: 10 }}></Text>
-          <Text style={{ color: "#ccc", fontSize: 20, margin: 10 }}>
-            예약시간 :
-          </Text>
-          <Text style={{ color: "#ccc", fontSize: 20, margin: 10 }}>
-            예약한 좌석 :
-          </Text>
-        </TouchableOpacity>
+        <View style={{ flex: 4.5 }}>
+          <PreviewLayout
+            selectedValue={direction}
+            values={["예약 내역","카페 위치", "좌석 위치 안내도"]}
+            setSelectedValue={setDirection}
+            style={getConfirmReservationStyle.contentLayout}
+          >
+            <FlatList
+              keyExtractor={(item) => item.idx}
+              data={imgArr}
+              style={getConfirmReservationStyle.picArea}
+              renderItem={({ item }) => (
+                <TouchableOpacity>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      margin: 10,
+                    }}
+                  >
+                    <Image style={getConfirmReservationStyle.image} source={item.src} />
+                    <Text style={{ color: "#ccc", fontSize: 20, margin: 15 }}>카페 주소 :</Text>
+                    <Text style={{ color: "#ccc", fontSize: 20, margin: 15 }}>이용 가능 시간 :</Text>
+                    <Text style={{ color: "#ccc", fontSize: 20, margin: 15 }}>예약금 수수료 여부 :</Text>
+                    <Text style={{ color: "#ccc", fontSize: 15, margin: 15 }}>*매장 입장시 예약화면을 보여주시면 됩니다.
+                    좌석 이동은 카페 직원께 문의 부탁드립니다.
+                    이용 완료시 본인의 물건이 없는지 확인해주시고 다음 사람을 위해 좌석을 깨끗이 정리해주시기 바랍니다.</Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+              numColumns={3}
+            />
+          </PreviewLayout>
+        </View>
+
+        <View style={getConfirmReservationStyle.btnContainer}>
+          
+        </View>
       </View>
-    </KeyboardAvoidingView>
+    </>
   );
 }
+
+function CafeTable(props) {
+  const [cafeName, setCafeName] = useState(props.name);
+  const [cafeLocation, setCafeLocation] = useState(props.location);
+  const [cafeInformation, setCafeInformaion] = useState(props.information);
+
+  return (
+    <>
+      <View style={getCafeTableStyle.container}>
+        <View style={getCafeTableStyle.imageContainer}>
+          <View style={getCafeTableStyle.image}>
+            <Image
+              source={require("../../img/coffeebayLogo_test.jpg")}
+              style={getConfirmReservationStyle.cafeLogo}
+            />
+            
+          </View>
+        </View>
+        <View style={getCafeTableStyle.contentContainer}>
+          <View style={getCafeTableStyle.textContent}>
+            <Text style={getCafeTableStyle.nameText}>{cafeName}</Text>
+            <Text style={getCafeTableStyle.contentText}>{cafeLocation}</Text>
+            <Text style={getCafeTableStyle.contentText}>{cafeInformation}</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const PreviewLayout = ({
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={{ marginBottom: 10, fontSize: 24 }}></Text>
+    <View style={getConfirmReservationStyle.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[
+            getConfirmReservationStyle.button,
+            selectedValue === value && getConfirmReservationStyle.selected,
+          ]}
+        >
+          <Text
+            style={[
+              getConfirmReservationStyle.buttonLabel,
+              selectedValue === value && getConfirmReservationStyle.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    {(() => {
+      if (selectedValue === "예약 내역")
+        return <View style={getConfirmReservationStyle.container}>{children}</View>;
+      else
+        return (
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Image
+              source={require("../../img/anySeatPic_text.png")}
+              style={getConfirmReservationStyle.seatPic}
+            />
+            
+          </View>
+        );
+    })()}
+  </View>
+);
 
 export default ConfirmReservationScreen;
--- styles/screens/ConfirmReservationStyle.js
+++ styles/screens/ConfirmReservationStyle.js
@@ -1,66 +1,87 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+  },
+  contentLayout: {
     flex: 3,
-    backgroundColor: "#fff",
+  },
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    marginHorizontal: "1%",
+    marginBottom: 5,
+    minWidth: "48%",
+    textAlign: "center",
+  },
+  selected: {
+    backgroundColor: "#001D44",
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "black",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  imageContainer: {
+    width: 140,
+    height: 140,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    borderColor: "#ddd",
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain",
+    borderWidth: 1,
+  },
+  cafeLogo: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cafeTitle: {
+    fontSize: 25,
+  },
+  seatPic: {
+    width: "100%",
+    height: "75%",
+  },
+  picArea: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 10,
+  },
+  btnContainer: {
     alignItems: "center",
     justifyContent: "center",
   },
-
-  homeText: {
-    marginTop: "20%",
-    marginBottom: "2%",
-  },
-
-  btnInfoReservation: {
-    width: Dimensions.get("window").width,
-    height: "50%",
-    //borderWidth: 2,
-    borderColor: "#ccc",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    backgroundColor: "white",
-    marginTop: "10%",
-    marginBottom: "5%",
-    paddingLeft: "2%",
-  },
-
-  btnNearbyCafe: {
-    width: Dimensions.get("window").width,
-    height: "15%",
-    borderTopWidth: 2,
-    borderBottomWidth: 1,
-    borderColor: "#ccc",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-
-  btnConfirmReservation: {
-    width: Dimensions.get("window").width,
-    height: "15%",
-    //borderTopWidth: 2,
-    //borderBottomWidth: 2,
-    borderColor: "#ccc",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-    borderRadius: 8,
-    backgroundColor: "white",
-    width: "94%",
-    marginLeft: "43%",
-  },
-
-  btnMyPage: {
-    width: Dimensions.get("window").width,
-    height: "15%",
-    borderTopWidth: 1,
-    borderBottomWidth: 2,
-    borderColor: "#ccc",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+  listContainer: {
+    marginHorizontal: "8%",
+    marginVertical: "10%",
   },
 });
 
