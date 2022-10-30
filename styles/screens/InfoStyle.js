aa15236fdbac4ee5fa0e0eab6e5ce7e4e85ac3f4
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-10-30 18:12:58 +0900

    change resizemode into contain

--- styles/screens/InfoStyle.js
+++ styles/screens/InfoStyle.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
   seatPic: {
-    resizeMode: "stretch",
+    resizeMode: "contain",
     width: "100%",
     height: "75%",
   },
--- styles/screens/ReserveStyle.js
+++ styles/screens/ReserveStyle.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   seatPic: {
-    resizeMode: "stretch",
+    resizeMode: "contain",
     width: "100%",
     height: "75%",
   },
