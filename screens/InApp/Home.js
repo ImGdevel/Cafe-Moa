f474c5b0108dbe611fcda9efbcd82f2c5e950652
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-10-30 13:15:15 +0900

    set Absolute position

--- screens/InApp/Home.js
+++ screens/InApp/Home.js
@@ -14,87 +14,95 @@ import { getGeoLocation } from "../../lib/LocationService";
 
 function HomeScreen({ navigation }) {
   const [userData, setUserData] = useState();
-  const [reserveInfo,setReserveInfo] = useState();
-  const [reserveCafeInfo,setReserveCafeInfo] = useState();
-  const [reserveLoading,setReserveLoading] = useState(false); //카페데이터를 불러오는 도중 사용할 로딩
-  
+  const [reserveInfo, setReserveInfo] = useState();
+  const [reserveCafeInfo, setReserveCafeInfo] = useState();
+  const [reserveLoading, setReserveLoading] = useState(false); //카페데이터를 불러오는 도중 사용할 로딩
 
   useEffect(() => {
     LoadHomePage();
   }, [setUserData]);
 
-  const LoadHomePage = async() => {
-    if(userData == null){
-      await getUserProfile().then(async(data)=>{
-        setUserData(data);
-        if(data != null && data.reservation.cafeId != null){
-          let reserve_cafe = await getCafeData(data.reservation.cafeId);
-          console.log(reserve_cafe);
-          setReserveCafeInfo(reserve_cafe);
-          setReserveLoading(true);
-        }
-      }).catch((err)=>{
-        console.log("잘못된 접근입니다.",err);
-        //navigation.replace("Auth"); //로그인 가능하면 풀어도 됩니다.
-      })
+  const LoadHomePage = async () => {
+    if (userData == null) {
+      await getUserProfile()
+        .then(async (data) => {
+          setUserData(data);
+          if (data != null && data.reservation.cafeId != null) {
+            let reserve_cafe = await getCafeData(data.reservation.cafeId);
+            console.log(reserve_cafe);
+            setReserveCafeInfo(reserve_cafe);
+            setReserveLoading(true);
+          }
+        })
+        .catch((err) => {
+          console.log("잘못된 접근입니다.", err);
+          //navigation.replace("Auth"); //로그인 가능하면 풀어도 됩니다.
+        });
     }
     let location = await getGeoLocation();
     let cafe = await getCafeDatabaseAd();
-  }
-  
+  };
 
-
-
-
-  const ReservationsHistory = () =>{
- 
-
-    const onConfirmReservation = () =>{
-      if(reserveCafeInfo != null){
+  const ReservationsHistory = () => {
+    const onConfirmReservation = () => {
+      if (reserveCafeInfo != null) {
         navigation.navigate("ConfirmReservation", {
           cafeData: reserveCafeInfo,
           userData: userData,
-        })
+        });
       }
-    }
+    };
     const confirmReservationUI = (
       <>
         <View style={getHomeStyle.infoContentContainer}>
           <View style={getCafeTableStyle.imageContainer}>
             <Image
-              source={(reserveLoading)?{uri:reserveCafeInfo.getLogo()} : {}}
+              source={reserveLoading ? { uri: reserveCafeInfo.getLogo() } : {}}
               style={getHomeStyle.image}
             />
           </View>
-        <View>
-        <Text style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}>
-          {(reserveLoading) ? userData.Name : "" }
-        </Text>
-        <Text style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}>
-          {(reserveLoading) ? reserveCafeInfo.getName() : "" }
-        </Text>
-        <Text style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}>
-          {(reserveLoading) ? userData.reservation.seatNumber : ""}번 좌석
-        </Text>
-        <Text style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}>
-          {(reserveLoading) ? userData.reservation.time : ""}시
-        </Text>
-        <TouchableOpacity
-          onPress={onConfirmReservation}>
-          <Text style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}>
-            {"\n\t\t\t\t\t\t"}▶ 내역 확인하기
-          </Text>
-        </TouchableOpacity>
+          <View>
+            <Text
+              style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}
+            >
+              {reserveLoading ? userData.Name : ""}
+            </Text>
+            <Text
+              style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}
+            >
+              {reserveLoading ? reserveCafeInfo.getName() : ""}
+            </Text>
+            <Text
+              style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}
+            >
+              {reserveLoading ? userData.reservation.seatNumber : ""}번 좌석
+            </Text>
+            <Text
+              style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}
+            >
+              {reserveLoading ? userData.reservation.time : ""}시
+            </Text>
+            <TouchableOpacity onPress={onConfirmReservation}>
+              <Text
+                style={{ color: "#001D44", fontSize: 20, marginHorizontal: 20 }}
+              >
+                {"\n\t\t\t\t\t\t"}▶ 내역 확인하기
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
       </>
-    )
-    if(reserveCafeInfo == null){
-      return(<><Text> 예약 내역이 없습니다</Text></>)
-    }else{
-      return confirmReservationUI
+    );
+    if (reserveCafeInfo == null) {
+      return (
+        <>
+          <Text> 예약 내역이 없습니다</Text>
+        </>
+      );
+    } else {
+      return confirmReservationUI;
     }
-  }
+  };
 
   return (
     <KeyboardAvoidingView style={getHomeStyle.container}>
@@ -105,16 +113,19 @@ function HomeScreen({ navigation }) {
       </View>
 
       <View style={getHomeStyle.contentContainer}>
-        <View>
-          {/**다른 홈내용 */}          
-        </View>
+        <View>{/**다른 홈내용 */}</View>
         <View style={getHomeStyle.btnInfoReservation}>
           <View style={getHomeStyle.infoContainer}>
-            <Text style={{ color: "white", fontSize: 30, marginHorizontal: 10 }}> 예약내역 </Text>
+            <Text
+              style={{ color: "white", fontSize: 30, marginHorizontal: 10 }}
+            >
+              {" "}
+              예약내역{" "}
+            </Text>
           </View>
-            <ReservationsHistory/>
-          </View>
-        
+          <ReservationsHistory />
+        </View>
+
         {/*___
         <TouchableOpacity
           style={getHomeStyle.btnNearbyCafe}
