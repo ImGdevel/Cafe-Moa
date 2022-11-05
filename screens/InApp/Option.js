6f956e8ff2cb271ceeb92f6bec510c6748c06085
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-11-05 16:20:40 +0900

    Add SwitchUI in optionScreen

--- screens/InApp/Option.js
+++ screens/InApp/Option.js
@@ -1,35 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { Image, TouchableOpacity, View, Text, ScrollView } from "react-native";
+import {
+  Image,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+  Switch,
+} from "react-native";
 
 import getOptionStyle from "../../styles/screens/OptionStyle";
 import IonIcons from "react-native-vector-icons/Ionicons";
 
 function MyPageScreen({ navigation, route }) {
+  const [isPushEnabled, setIsPushEnabled] = useState(false);
+  const [isDarkEnabled, setIsDarkEnabled] = useState(false);
+  const PushNotiSwitch = () =>
+    setIsPushEnabled((previousState) => !previousState);
+  const ThemeSwitch = () => setIsDarkEnabled((previousState) => !previousState);
+
   return (
     <ScrollView style={getOptionStyle.container}>
       <View style={getOptionStyle.subtitleHeader}>
         <Text sytle={{ fontSize: 20 }}>일반</Text>
       </View>
-      <TouchableOpacity style={getOptionStyle.optionBtn}>
-        <Text style={{ color: "black", fontWeight: "500", fontSize: 20 }}>
-          <IonIcons
-            name="notifications-circle-outline"
-            style={{ fontSize: 20, color: "#001D44" }}
-          ></IonIcons>
-          {"\t"}푸시 알림
-        </Text>
-        <Text style={{ color: "#bbb" }}>푸시 알림 수신 기능 On/Off</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={getOptionStyle.optionBtn}>
-        <Text style={{ color: "black", fontWeight: "500", fontSize: 20 }}>
-          <IonIcons
-            name="contrast-outline"
-            style={{ fontSize: 20, color: "#001D44" }}
-          ></IonIcons>
-          {"\t"}테마
-        </Text>
-        <Text style={{ color: "#bbb" }}>라이트모드 / 다크 모드</Text>
-      </TouchableOpacity>
+      <View style={getOptionStyle.optionBtn}>
+        <View>
+          <Text style={{ color: "black", fontWeight: "500", fontSize: 20 }}>
+            <IonIcons
+              name="notifications-circle-outline"
+              style={{ fontSize: 20, color: "#001D44" }}
+            ></IonIcons>
+            {"\t"}푸시 알림
+          </Text>
+          <Text style={{ color: "#bbb" }}>푸시 알림 수신 기능 On/Off</Text>
+        </View>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isPushEnabled ? "#f4f3f4" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={PushNotiSwitch}
+          value={isPushEnabled}
+        />
+      </View>
+      <View style={getOptionStyle.optionBtn}>
+        <View>
+          <Text style={{ color: "black", fontWeight: "500", fontSize: 20 }}>
+            <IonIcons
+              name="contrast-outline"
+              style={{ fontSize: 20, color: "#001D44" }}
+            ></IonIcons>
+            {"\t"}테마
+          </Text>
+          <Text style={{ color: "#bbb" }}>라이트모드 / 다크 모드</Text>
+        </View>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkEnabled ? "#f4f3f4" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={ThemeSwitch}
+          value={isDarkEnabled}
+        />
+      </View>
     </ScrollView>
   );
 }
