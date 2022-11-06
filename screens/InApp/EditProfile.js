f7f79c3b859348387efd9cc4bca3e502891bb5a4
ksj000625 <68013705+ksj000625@users.noreply.github.com>
2022-11-06 16:26:24 +0900

    Setting UI of EditProfilieScreen

--- screens/InApp/EditProfile.js
+++ screens/InApp/EditProfile.js
@@ -1,20 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
 import {
   Image,
   TouchableOpacity,
   View,
   Text,
+  TextInput,
   KeyboardAvoidingView,
-  TouchableWithoutFeedback,
 } from "react-native";
 
 import getEditProfileStyle from "../../styles/screens/EditProfileStyle";
 
 function EditProfileScreen({ navigation, route }) {
+  const nameInputRef = createRef();
+  const emailInputRef = createRef();
+  const pwInputRef = createRef();
+
+  function ConfirmEditProfile() {
+    //navigation.navigate()
+  }
+
   return (
-    <View>
-      <View></View>
-    </View>
+    <KeyboardAvoidingView style={getEditProfileStyle.container}>
+      <TouchableOpacity style={getEditProfileStyle.ProfilePicBtn} onPress={{}}>
+        <Image
+          style={{ width: 180, height: 180 }}
+          source={require("../../img/initialProfile.jpg")}
+        ></Image>
+      </TouchableOpacity>
+      <View style={getEditProfileStyle.InputField}>
+        <View style={getEditProfileStyle.ChangeBtn}>
+          <Text style={getEditProfileStyle.FieldText}>닉네임</Text>
+          <TextInput
+            ref={nameInputRef}
+            style={getEditProfileStyle.textInput}
+            placeholder={"닉네임"}
+            onChangeText={() => {}}
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            returnKeyType="next"
+            onSubmitEditing={() => {}}
+          />
+        </View>
+        <View style={getEditProfileStyle.ChangeBtn}>
+          <Text style={getEditProfileStyle.FieldText}>이메일</Text>
+          <TextInput
+            ref={emailInputRef}
+            style={getEditProfileStyle.textInput}
+            placeholder={"이메일"}
+            onChangeText={() => {}}
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            returnKeyType="next"
+            onSubmitEditing={() => {}}
+          />
+        </View>
+        <View style={getEditProfileStyle.ChangeBtn}>
+          <Text style={getEditProfileStyle.FieldText}>비밀번호</Text>
+          <TextInput
+            ref={pwInputRef}
+            style={getEditProfileStyle.textInput}
+            placeholder={"비밀번호"}
+            onChangeText={() => {}}
+            secureTextEntry={true}
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            returnKeyType="next"
+            onSubmitEditing={() => {}}
+          />
+        </View>
+      </View>
+      <View
+        style={{ width: "100%", alignItems: "center", backgroundColor: "#ccc" }}
+      >
+        <TouchableOpacity
+          style={getEditProfileStyle.confirmEditBtn}
+          onPress={ConfirmEditProfile}
+        >
+          <Text style={{ color: "white", fontSize: 20 }}>
+            개인정보 수정하기
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
--- styles/screens/EditProfileStyle.js
+++ styles/screens/EditProfileStyle.js
@@ -1,5 +1,61 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+  ProfilePicBtn: {
+    width: 180,
+    height: 180,
+    marginVertical: "10%",
+    borderWidth: 1,
+    borderRadius: "100%",
+    borderColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  InputField: {
+    flex: 1,
+    backgroundColor: "#ccc",
+    width: "100%",
+    paddingTop: 5,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  ChangeBtn: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  FieldText: {
+    fontSize: 18,
+    paddingVertical: 6,
+    paddingLeft: 10,
+  },
+  confirmEditBtn: {
+    width: "80%",
+    height: 50,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#001D44",
+    marginBottom: 15,
+  },
+  textInput: {
+    width: "90%",
+    height: 40,
+    left: 10,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+  },
+});
 
 export default styles;
