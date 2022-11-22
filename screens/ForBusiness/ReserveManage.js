c2b714b0f869891f8a487cc3995f129c455b6490
LTT <68013705+ksj000625@users.noreply.github.com>
2022-11-22 17:48:06 +0900

    Setting reservations layout

--- screens/ForBusiness/ReserveManage.js
+++ screens/ForBusiness/ReserveManage.js
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
+import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
 
 import getManageStyle from "../../styles/screens/ReserveManageStyle";
 
 function ReserveManageScreen({ navigation }) {
   return (
-    <View>
-      <View></View>
-    </View>
+    <ScrollView style={getManageStyle.container}>
+      <View style={getManageStyle.manualContianer}></View>
+      <View>
+        <View style={getManageStyle.timeArea}>
+          <Text style={getManageStyle.timeText}>11시</Text>
+        </View>
+        <ScrollView horizontal={true} style={getManageStyle.numContainer}>
+          <View style={getManageStyle.setNumBox}>
+            <Text style={{ color: "#001D44" }}>7번 좌석</Text>
+          </View>
+          <View style={getManageStyle.setNumBox}>
+            <Text style={{ color: "#001D44" }}>8번 좌석</Text>
+          </View>
+          <View style={getManageStyle.setNumBox}>
+            <Text style={{ color: "#001D44" }}>9번 좌석</Text>
+          </View>
+        </ScrollView>
+        <View style={getManageStyle.timeArea}>
+          <Text style={getManageStyle.timeText}>12시</Text>
+        </View>
+        <ScrollView horizontal={true} style={getManageStyle.numContainer}>
+          <View style={getManageStyle.setNumBox}>
+            <Text style={{ color: "#001D44" }}>1번 좌석</Text>
+          </View>
+          <View style={getManageStyle.setNumBox}>
+            <Text style={{ color: "#001D44" }}>6번 좌석</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 }
 
--- styles/screens/ReserveManageStyle.js
+++ styles/screens/ReserveManageStyle.js
@@ -1,5 +1,50 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    width: "100%",
+    height: "100%",
+  },
+  manualContianer: {
+    width: "100%",
+    height: 120,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  timeArea: {
+    width: 70,
+    height: 30,
+    justifyContent: "center",
+    backgroundColor: "#001D44",
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+  },
+  timeText: {
+    fontSize: "18",
+    color: "white",
+  },
+  numContainer: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "white",
+    flexDirection: "row",
+    paddingLeft: 5,
+    marginBottom: 10,
+  },
+  setNumBox: {
+    width: 70,
+    height: 70,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: "#001D44",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default styles;
