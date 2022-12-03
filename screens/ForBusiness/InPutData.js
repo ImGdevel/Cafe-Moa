f141b223859e76f468ff4c5a6f53ad42ca93ca0e
LTT <68013705+ksj000625@users.noreply.github.com>
2022-12-03 18:06:35 +0900

    Complete Setting Layout

--- screens/ForBusiness/InPutData.js
+++ screens/ForBusiness/InPutData.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   KeyboardAvoidingView,
   Image,
+  ScrollView,
 } from "react-native";
 import { getRandomCafeData } from "../../lib/TestSample";
 import { getGeoLocation } from "../../lib/LocationService";
@@ -25,10 +26,10 @@ function InPutDataScreen({ navigation }) {
     latitude: 37,
     longitude: 127,
   });
-  const [cafeImfo, setcafeImfo] = useState("");
+  const [cafeInfo, setcafeImfo] = useState("");
   const cafeNameInputRef = createRef();
   const cafeLocationInputRef = createRef();
-  const cafeImfoInputRef = createRef();
+  const cafeInfoInputRef = createRef();
   const cafeTimeRef = createRef();
   const [local, setLocal] = useState("");
 
@@ -68,7 +69,166 @@ function InPutDataScreen({ navigation }) {
 
   return (
     <KeyboardAvoidingView style={getInputStyle.container}>
-      <View style={{ flex: 3 }}></View>
+      <View style={getInputStyle.headerContainer}>
+        <Text style={{ fontSize: 23, fontWeight: "bold", color: "#001D44" }}>
+          Create MOA Cafe
+        </Text>
+      </View>
+      <View style={getInputStyle.contentContainer}>
+        <View style={getInputStyle.cafeInfoHeader}>
+          <View style={getInputStyle.cafeImagePicker}>
+            <Image
+              source={require("../../img/coffeebayLogo_test.jpg")}
+              style={{ width: "100%", height: "100%", borderRadius: 15 }}
+            />
+          </View>
+          <View style={getInputStyle.nameInputContainer}>
+            <TextInput
+              ref={cafeNameInputRef}
+              style={getInputStyle.nameTextInput}
+              placeholder={"카페이름"}
+              onChangeText={(cafeName) => setcCafeName(cafeName)}
+              autoCapitalize="none"
+              blurOnSubmit={false}
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                cafeLocationInputRef.current &&
+                cafeLocationInputRef.current.focus()
+              }
+            />
+            <TouchableOpacity style={getInputStyle.locationButton}>
+              <Text
+                style={{ color: "#001D44", fontSize: 20, fontWeight: "bold" }}
+              >
+                카페 위치
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{ width: "100%", height: "5%", backgroundColor: "white" }}
+        ></View>
+        <View style={getInputStyle.cafeTimeContainer}>
+          <View style={getInputStyle.oncTimeContainer}>
+            <View
+              style={{
+                width: "100%",
+                height: "50%",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                paddingTop: 8,
+              }}
+            >
+              <Text
+                style={{ color: "#001D44", fontSize: 20, fontWeight: "bold" }}
+              >
+                Open
+              </Text>
+              <View
+                style={{
+                  width: "100%",
+                  height: "50%",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: 8,
+                }}
+              >
+                <TextInput
+                  ref={cafeNameInputRef}
+                  style={getInputStyle.timeTextInput}
+                  placeholder={"시간"}
+                  onChangeText={(text) => setcCafeName(text)}
+                  autoCapitalize="none"
+                  keyboardType="number-pad"
+                  blurOnSubmit={false}
+                  returnKeyType="next"
+                  onSubmitEditing={() =>
+                    cafeLocationInputRef.current &&
+                    cafeLocationInputRef.current.focus()
+                  }
+                />
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              width: "30%",
+              height: "100%",
+              backgroundColor: "white",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{ color: "#001D44", fontSize: 30, fontWeight: "bold" }}
+            >
+              ~
+            </Text>
+          </View>
+          <View style={getInputStyle.oncTimeContainer}>
+            <View
+              style={{
+                width: "100%",
+                height: "50%",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                paddingTop: 8,
+              }}
+            >
+              <Text
+                style={{ color: "#001D44", fontSize: 20, fontWeight: "bold" }}
+              >
+                Close
+              </Text>
+              <View
+                style={{
+                  width: "100%",
+                  height: "50%",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  padding: 8,
+                }}
+              >
+                <TextInput
+                  ref={cafeNameInputRef}
+                  style={getInputStyle.timeTextInput}
+                  placeholder={"시간"}
+                  onChangeText={(text) => setcCafeName(text)}
+                  autoCapitalize="none"
+                  keyboardType="number-pad"
+                  blurOnSubmit={false}
+                  returnKeyType="next"
+                  onSubmitEditing={() =>
+                    cafeLocationInputRef.current &&
+                    cafeLocationInputRef.current.focus()
+                  }
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={getInputStyle.seatImagePickerContainer}>
+          <View style={getInputStyle.setImagePicker}>
+            <Image
+              source={require("../../img/anySeatPic_text.png")}
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: 15,
+                resizeMode: "contain",
+              }}
+            />
+          </View>
+        </View>
+      </View>
+      <View style={getInputStyle.submitBtnContainer}>
+        <TouchableOpacity style={getInputStyle.submitButton}>
+          <Text style={{ color: "white", fontSize: 25, fontWeight: "bold" }}>
+            카페 만들기
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* <View style={{ flex: 3 }}></View>
       <View style={getInputStyle.contentArea}>
         <View style={getInputStyle.titleText}>
           <Text style={{ fontWeight: "900", fontSize: 50 }}> M O A </Text>
@@ -137,7 +297,7 @@ function InPutDataScreen({ navigation }) {
           </TouchableOpacity>
         </View>
       </View>
-      <View style={{ flex: 4 }}></View>
+      <View style={{ flex: 4 }}></View> */}
     </KeyboardAvoidingView>
   );
 }
