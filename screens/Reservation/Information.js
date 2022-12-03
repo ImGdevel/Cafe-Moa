46d48bba1e1ce400f07a3cfd82817ee81e76df32
CreaterRacon <62339794+WooSeungHwa@users.noreply.github.com>
2022-12-03 13:40:22 +0900

    aa

--- screens/Reservation/Information.js
+++ screens/Reservation/Information.js
@@ -51,7 +51,7 @@ function InformationScreen({ navigation, route }) {
 
   return (
     <>
-      <View style={getInfoStyle.container}>
+      <View style={getInfoStyle.mainContainer}>
         <View style={getFindStyle.container}>
           <View style={getFindStyle.contentContainer}>
             <CafeTable
