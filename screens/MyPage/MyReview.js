cd348a7637510e2e5d7f646cbb14ee63bb59bbd9
KimSeongjin <darkmoon625@naver.com>
2022-12-06 21:12:56 +0900

    Setting UI of MyReview

--- screens/MyPage/MyReview.js
+++ screens/MyPage/MyReview.js
@@ -5,21 +5,108 @@ import {
   View,
   Text,
   ScrollView,
+  TouchableOpacity,
 } from "react-native";
 
 import getMyReviewStyle from "../../styles/screens/MyReviewStyle";
+import getReviewStyle from "../../styles/components/ReviewStyle";
 
-import Ionicons from "react-native-vector-icons/Ionicons";
+import Star from "../../Components/Star";
+
 import { getCafeDatas } from "../../lib/CafeService";
 
 function MyReviewScreen({ navigation, route }) {
-  const { userData: userData } = route.params;
+  const [userData, setUserData] = useState(route.params.userData);
+
+  const [userID, setUserID] = useState(null);
+  const [userName, setUserName] = useState("user");
+  const [date, setDate] = useState("date");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    getImages(userData.id);
+  }, []);
+
+  async function getImages(id) {
+    if (id != null) {
+      console.log("이미지 출력");
+      const img = await getImage("User", id, "profile");
+      setImage({ uri: img });
+    } else {
+      setImage(require("../../img/initialProfile.jpg"));
+    }
+  }
 
   return (
     <View style={getMyReviewStyle.container}>
-      <View style={getMyReviewStyle.contentContainer}>
-        <ScrollView></ScrollView>
-      </View>
+      <ScrollView style={getMyReviewStyle.contentContainer}>
+        <View style={getReviewStyle.reviewContentContainer}>
+          <View style={getReviewStyle.reviewContentHeader}>
+            <Image
+              style={{ width: 50, height: 50, borderRadius: 50 }}
+              source={image}
+            />
+            <View style={getMyReviewStyle.reviewHead}>
+              <View style={{ justifyContent: "space-evenly" }}>
+                <View>
+                  <Star value={5} />
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginLeft: 0,
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <Text style={{ fontSize: 15, color: "gray" }}>
+                    {userName}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      color: "gray",
+                      marginLeft: 10,
+                      marginTop: 2,
+                    }}
+                  >
+                    {date}
+                  </Text>
+                </View>
+              </View>
+              <View>
+                <TouchableOpacity style={getMyReviewStyle.deleteButton}>
+                  <Text style={{ fontSize: 14, color: "gray" }}>삭제</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <Text style={getReviewStyle.reviewContent}>
+            이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게
+            뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데
+            이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게
+            뭔데 이게 뭔데 이게 뭔데 이게 뭔데 이게 뭔데
+          </Text>
+          <View style={getMyReviewStyle.reviewedCafeInfoContainer}>
+            <View style={getMyReviewStyle.cafeLogoContainer}>
+              <Image
+                style={{ width: "100%", height: "100%", borderRadius: 15 }}
+                source={require("../../img/coffeebayLogo_test.jpg")}
+              />
+            </View>
+            <View style={getMyReviewStyle.cafeDataContainer}>
+              <View style={getMyReviewStyle.cafeData}>
+                <Text style={{ fontSize: 22 }}>--카페이름--</Text>
+              </View>
+              <View style={getMyReviewStyle.cafeData}>
+                <Text style={{ fontSize: 18, color: "gray" }}>
+                  --카페위치--
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- styles/screens/MyReviewStyle.js
+++ styles/screens/MyReviewStyle.js
@@ -9,6 +9,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  reviewedCafeInfoContainer: {
+    width: "100%",
+    height: 100,
+    flexDirection: "row",
+  },
+  reviewHead: {
+    paddingLeft: 15,
+    width: 310,
+    height: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  deleteButton: {
+    width: 50,
+    height: 25,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cafeLogoContainer: {
+    width: 90,
+    height: 90,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 15,
+  },
+  cafeDataContainer: {
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  cafeData: {
+    height: "50%",
+    justifyContent: "center",
+    paddingLeft: 10,
+  },
 });
 
 export default styles;
